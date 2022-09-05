b95ed744bdd3963c9552849d76b1b5e28437d5e7
TheVexTeamPublic <98670994+TheVexTeamPublic@users.noreply.github.com>
2022-09-05 11:52:40 -0400

    Update AutoEnhance.lua

--- AutoEnhance.lua
+++ AutoEnhance.lua
@@ -15,27 +15,28 @@ local First_Person_Lock = true
 
 local FOV = game.Workspace.CurrentCamera.FieldOfView
 
---local ChangeFOV = true
+local ChangeFOV = true
 
---                            X    Y      Z
 local PlayerIden = game.Players.LocalPlayer
-local char = PlayerIden.CharacterAdded:wait()
-local hum = char:WaitForChild("Humanoid")
-local rootpart,head = char:WaitForChild("HumanoidRootPart"),char:WaitForChild("Head")
-game:GetService("RunService"):BindToRenderStep("CameraOffset",Enum.RenderPriority.Camera.Value-1,function()
-	hum.CameraOffset = (rootpart.CFrame+Vector3.new(0,1.25,-1.5)):pointToObjectSpace(head.CFrame.p)
-end)
 
-local RunFOV = 120
-local RegFOV = 95
+local char = PlayerIden.Character
+
+local hum = char:WaitForChild("Humanoid")
+
+local head = char:FindFirstChild("Head")
+
+local Offset = Vector3.new(0, -0.125, -1)
+
+hum.CameraOffset = Offset
+
+local FOV = 120
 -----------------------------------------------------------------------------------
 
---[[if ChangeFOV == true then
+if ChangeFOV == true then
 game.Workspace.CurrentCamera.FieldOfView = FOV
 end
-]]
 
---PlayerIden.Character.Humanoid.CameraOffset = CamOffset
+PlayerIden.Character.Humanoid.CameraOffset = CamOffset
 if First_Person_Lock == true then
 PlayerIden.CameraMode = "LockFirstPerson"
 end
@@ -69,18 +70,3 @@ game:GetService("RunService").RenderStepped:Connect(function()
 	end
 end)
 end
-
--- Running
-local uis = Game:GetService("UserInputService")
-uis.InputBegan:connect(function(inst)
-   if inst.KeyCode == Enum.KeyCode.LeftShift then
-       game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed = game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed + 5
-		FOV = RunFOV	
-   end
-end)
-uis.InputEnded:connect(function(inst)
-   if inst.KeyCode == Enum.KeyCode.LeftShift then
-       game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed = game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed - 5
-			FOV = RegFOV
-   end
-end)
